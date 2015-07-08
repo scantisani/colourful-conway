e3022789f738fc72c04b8ee5dcf82d2ca8d169f3
Scott Cantisani <scantisani@gmail.com>
2015-07-08 16:10:38 +0100

    Created Game class that will deal with game logic

---
+++ game.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using std::cout; using std::endl;
+
+#include "game.h"
+
+void Game::createCell() {
+	cout << "Cell created!" << endl;
+}
+
+void Game::destroyCell() {
+	cout << "Cell destroyed!" << endl;
+}
---
+++ game.h
@@ -0,0 +1,10 @@
+#ifndef GAME_H
+#define GAME_H
+
+class Game {
+public:
+	void createCell();
+	void destroyCell();
+};
+
+#endif
