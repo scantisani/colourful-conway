ad3cf8948b38bd753c0bc7ea51d9869220404905
Scott Cantisani <scantisani@gmail.com>
2015-07-08 12:37:53 +0100

    Clicking on a grid cell now fills it

--- grid.cpp
+++ grid.cpp
@@ -7,23 +7,26 @@ using std::cerr;
 using std::runtime_error;
 
 #include <string>
-using std::string;
+using std::string; using std::to_string;
 
 // Screen dimension constants
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
 // Grid cell size constant
-const int CELL_SIZE = 5;
+const int CELL_SIZE = 10;
 
 // SDL initialization and termination functions
 void initSDL();
-void closeSDL();
+void closeSDL(SDL_Window* window, SDL_Renderer* renderer);
 
 // SDL window initializer function
 SDL_Window* initWindow();
 // SDL renderer initializer function
-SDL_Renderer* initRender();
+SDL_Renderer* initRenderer(SDL_Window* window);
+
+// function to draw the grid for the cells
+void drawGrid(SDL_Renderer* renderer);
 
 void initSDL() {
 	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
@@ -61,30 +64,50 @@ void closeSDL(SDL_Window* window, SDL_Renderer* renderer) {
 	SDL_Quit();
 }
 
+void drawGrid(SDL_Renderer* renderer) {
+	// Clear window/draw white background
+	SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+	SDL_RenderClear(renderer);
+
+	// Draw black horizontal lines
+	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);		
+	for (int i = 0; i < SCREEN_HEIGHT; i += CELL_SIZE)
+		SDL_RenderDrawLine(renderer, 0, i, SCREEN_WIDTH, i);
+	// Draw black vertical lines
+	for (int i = 0; i < SCREEN_WIDTH; i += CELL_SIZE)
+		SDL_RenderDrawLine(renderer, i, 0, i, SCREEN_HEIGHT);
+}
+
+void fillCell(SDL_Renderer* renderer, int x, int y) {
+	SDL_Rect cell;
+	// x and y are top left corner of cell mouse pointer is in
+	cell.x = x - (x % CELL_SIZE) + 1; // 1-pixel buffer so as not to erase grid lines
+	cell.y = y - (y % CELL_SIZE) + 1;
+	cell.w = CELL_SIZE - 1;
+	cell.h = CELL_SIZE - 1;
+
+	SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+
+	SDL_RenderFillRect(renderer, &cell);
+}
+
 int main(int argc, char const *argv[]) {
 	try {
 		SDL_Event event;
 		SDL_Window* window = initWindow();
 		SDL_Renderer* renderer = initRenderer(window);
 
+		drawGrid(renderer);
+
 		// While user hasn't closed the window
 		while (event.type != SDL_QUIT) {
 			// Poll for events on queue
 			SDL_PollEvent(&event);
 
-			// Clear screen
-			SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-			SDL_RenderClear(renderer);
-
-			// Draw black horizontal lines
-			SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);		
-			for (int i = 0; i < SCREEN_HEIGHT; i += CELL_SIZE)
-				SDL_RenderDrawLine(renderer, 0, i, SCREEN_WIDTH, i);
-			// Draw black vertical lines
-			for (int i = 0; i < SCREEN_WIDTH; i += CELL_SIZE)
-				SDL_RenderDrawLine(renderer, i, 0, i, SCREEN_HEIGHT);
-
-			// Update screen
+			if (event.button.type == SDL_MOUSEBUTTONDOWN) {
+				fillCell(renderer, event.button.x, event.button.y);
+			}
+			// Refresh window
 			SDL_RenderPresent(renderer);
 		}
 
