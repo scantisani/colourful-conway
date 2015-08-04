e3e0fab88e534dcdc0fa2655dcbd0ec85599a1e6
Scott Cantisani <scantisani@gmail.com>
2015-08-04 14:11:39 +0100

    Added unit tests for Game::step() verifying game adheres to Game of Life rules

--- game_test.cpp
+++ game_test.cpp
@@ -44,4 +44,120 @@ BOOST_AUTO_TEST_CASE(gameGetLiveCells_passesCorrectCellObjects) {
 	BOOST_CHECK_EQUAL(liveCells[1].y, 4);
 }
 
+BOOST_AUTO_TEST_CASE(gameStep_killsCells_withNoNeighbours) {
+	game.toggleCell(1, 1);
+	game.step();
+
+	BOOST_CHECK_EQUAL(game.getLiveCells().size(), 0);
+}
+
+BOOST_AUTO_TEST_CASE(gameStep_killsCells_withOneNeighbour) {
+	// cells with one neighbour (each other)
+	game.toggleCell(1, 1);
+	game.toggleCell(1, 2);
+
+	game.step();
+
+	BOOST_CHECK_EQUAL(game.getLiveCells().size(), 0);
+}
+
+BOOST_AUTO_TEST_CASE(gameStep_ignoresLiveCells_withTwoNeighbours) {
+	// cells that each have two neighbours
+	game.toggleCell(1, 1);
+	game.toggleCell(1, 2);
+	game.toggleCell(2, 1);
+
+	game.step();
+
+	vector<Cell> liveCells = game.getLiveCells();
+	int found = 0;
+
+	// check our original three cells are still alive
+	for (Cell cell : liveCells) {
+		if	(
+			(cell.x == 1 && cell.y == 1) ||
+			(cell.x == 1 && cell.y == 2) ||
+			(cell.x == 2 && cell.y == 1)
+			) {
+			++found;
+		}
+	}
+
+	BOOST_CHECK_EQUAL(found, 3);
+}
+
+BOOST_AUTO_TEST_CASE(gameStep_ignoresDeadCells_withTwoNeighbours) {
+	game.toggleCell(1, 1);
+	game.toggleCell(1, 3);
+	// dead cell: 1, 2
+
+	game.step();
+
+	vector<Cell> liveCells = game.getLiveCells();
+
+	for (Cell cell : liveCells)
+		BOOST_CHECK(!(cell.x == 1 && cell.y == 2));
+}
+
+
+BOOST_AUTO_TEST_CASE(gameStep_ignoresLiveCells_withThreeNeighbours) {
+	// cells that each have two neighbours
+	game.toggleCell(1, 1);
+	game.toggleCell(1, 2);
+	game.toggleCell(2, 1);
+
+	game.step();
+
+	vector<Cell> liveCells = game.getLiveCells();
+	int found = 0;
+
+	// check our original three cells are still alive
+	for (Cell cell : liveCells) {
+		if	(
+			(cell.x == 1 && cell.y == 1) ||
+			(cell.x == 1 && cell.y == 2) ||
+			(cell.x == 2 && cell.y == 1)
+			) {
+			++found;
+		}
+	}
+
+	BOOST_CHECK_EQUAL(found, 3);
+}
+
+BOOST_AUTO_TEST_CASE(gameStep_respawnsCells_withThreeNeighbours) {
+	game.toggleCell(1, 1);
+	game.toggleCell(1, 2);
+	game.toggleCell(2, 1);
+	// dead cell: 2, 2
+
+	game.step();
+
+	vector<Cell> liveCells = game.getLiveCells();
+	bool respawned = false;
+
+	for (Cell cell : liveCells) {
+		if (cell.x == 2 && cell.y == 2)
+			respawned = true;
+	}
+
+	BOOST_CHECK(respawned);
+}
+
+BOOST_AUTO_TEST_CASE(gameStep_killsCells_withFourNeighbours) {
+	game.toggleCell(2, 2);
+
+	game.toggleCell(1, 2);
+	game.toggleCell(3, 2);
+	game.toggleCell(2, 1);
+	game.toggleCell(2, 3);
+
+	game.step();
+
+	vector<Cell> liveCells = game.getLiveCells();
+
+	for (Cell cell : liveCells)
+		BOOST_CHECK(!(cell.x == 2 && cell.y == 2));
+}
+
 BOOST_AUTO_TEST_SUITE_END();
